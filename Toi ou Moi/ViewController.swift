c731ca79f8bc64eecc953543dae4b1a9b21e7225
Michel garlandat <michel.garlandat@me.com>
2019-05-28 11:46:28 +0200

    Initial Commit

---
+++ Toi ou Moi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toi ou Moi
+//
+//  Created by Michel Garlandat on 28/05/2019.
+//  Copyright © 2019 Michel Garlandat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
